c9c764fde2e6cdbf6d09577daf82e9bf4af9db70
Piyush <sab9861@gmail.com>
2023-09-15 00:32:49 +0530

    File uplodaing done....

--- JPADEMO/src/main/java/com/api/book/Controller/FileUploadController.java
+++ JPADEMO/src/main/java/com/api/book/Controller/FileUploadController.java
@@ -1,14 +1,34 @@
 package com.api.book.Controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 public class FileUploadController {
-	@PostMapping("/upload")
-	public ResponseEntity<String> uploadFile()
+	@Autowired
+	public FileUploadController obj;
+	
+	@PostMapping("/upload-file")
+	public ResponseEntity<String> uploadFile(@RequestParam("file") MultipartFile file)
 	{
-		return ResponseEntity.ok("working..");
+//		System.out.println(file.getName());
+//		System.out.println(file.getOriginalFilename());
+//		System.out.println(file.getSize());
+		
+//		return ResponseEntity.ok("working..");.
+		
+		try {
+			obj.uploadFile(file);
+			return ResponseEntity.ok("done...");
+		} catch (Exception e) {
+			// TODO: handle exception
+			e.printStackTrace();
+		}
+		return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("wrong");
 	}
 }
---
+++ JPADEMO/src/main/java/com/api/help/fileupload/FileUploadHelper.java
@@ -0,0 +1,43 @@
+package com.api.help.fileupload;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.multipart.MultipartFile;
+
+@Component
+public class FileUploadHelper {
+	public final String 
+	upload_dir="D:\\Spring_Boot_Work_Space\\JPADEMO\\src\\main\\resources\\static//Images";
+	
+	public boolean uploadFile(MultipartFile fi) {
+		boolean f=false;
+		
+		try {
+//			InputStream is=fi.getInputStream();
+//			byte arr[]=new byte[is.available()];
+//			is.read(arr);
+//			
+//			FileOutputStream fo=new FileOutputStream(upload_dir+File.separator+fi.getOriginalFilename());
+//			
+//			fo.flush();
+//			fo.close();
+			
+			Files.copy(fi.getInputStream(), Paths.get(upload_dir+File.pathSeparator+fi.getOriginalFilename()),StandardCopyOption.REPLACE_EXISTING);
+			
+			
+			f=true;
+			
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+		
+		return f;
+	}
+	
+}
